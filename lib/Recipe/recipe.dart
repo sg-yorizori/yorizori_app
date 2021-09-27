3e61d179334282190a63436077407f91c0a2c57f
huttzza <dobbie6174@gmail.com>
2021-09-27 19:07:36 +0900

    set mainpage

---
+++ lib/Recipe/recipe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Recipe extends StatefulWidget {
+  Recipe({Key? key}) : super(key: key);
+
+  @override
+  _RecipeState createState() => _RecipeState();
+}
+
+class _RecipeState extends State<Recipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
